72adb1a9c1ad50cde06bebbab05d2fc72eea1d56
Dmytro Turivnyi <dmytro_turivnyi@epam.com>
2019-04-27 14:40:57 +0000

    variables lesson

--- docker/main.tf
+++ docker/main.tf
@@ -1,14 +1,39 @@
-# Download the latest Ghost image
-resource "docker_image" "here_image_id" {
-  name = "ghost:latest"
+variable "container_name" {
+  description = "Name of container"
+  default = "blog"
+}
+variable "image" {
+  description = "image for container"
+  default = "ghost:latest"
+}
+variable "int_port" {
+  description = "internal port for container"
+  default = "2368"
+}
+variable "ext_port" {
+  description = "external port for container"
+  default = "80"
 }
 
+# Download the latest Ghost Image
+resource "docker_image" "image_id" {
+  name = "${var.image}"
+}
 # Start the Container
 resource "docker_container" "container_id" {
-  name  = "blog"
-  image = "${docker_image.here_image_id.latest}"
+  name  = "${var.container_name}"
+  image = "${docker_image.image_id.latest}"
   ports {
-    internal = "2368"
-    external = "80"
+    internal = "${var.int_port}"
+    external = "${var.ext_port}"
   }
+}
+
+#Output the IP Address of the Container
+output "IP Address" {
+  value = "${docker_container.container_id.ip_address}"
+}
+
+output "container_name" {
+  value = "${docker_container.container_id.name}"
 }
